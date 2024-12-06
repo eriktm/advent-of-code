b0aca0afb5e183beef83c8373e82c5e886cb72c1
Erik Hetland <erik@upheads.no>
2024-12-06 12:08:33 +0100

    add day06 part 2

--- 2024/day06.py
+++ 2024/day06.py
@@ -5,38 +5,54 @@ def get_coords(chart, item):
                 return x, y
 
 
-def next_dir(dx, dy):
-    if (dx, dy) == (-1, 0):
-        return (0, 1)
-    elif (dx, dy) == (0, 1):
-        return (1, 0)
-    elif (dx, dy) == (1, 0):
-        return (0, -1)
-    elif (dx, dy) == (0, -1):
-        return (-1, 0)
-
-
-def map_path(chart, start, dir):
-    px, py = start
-    dx, dy = dir
-    path = [start]
+def map_path(chart):
+    px, py = get_coords(chart, "^")
+    dx, dy = -1, 0
+    path = {((px, py), (dx, dy))}
 
     while True:
         nx, ny = px + dx, py + dy
         # print(f"{px=}, {py=}, {dx=}, {dy=}, {nx=}, {ny=}")
         if nx < 0 or nx > len(chart) - 1 or ny < 0 or ny > len(chart[0]) - 1:
-            return path
+            break
 
         if chart[nx][ny] == "#":
-            dx, dy = next_dir(dx, dy)
+            dx, dy = dy, -dx
             continue
 
+        coord = ((nx, ny), (dx, dy))
+        if coord in path:
+            return False, set()
+
+        path.add(coord)
         px, py = nx, ny
-        path.append((nx, ny))
+
+    return True, {coord for coord, _ in path}
+
+
+def find_new_obstacles(chart, path):
+    obstacles = set()
+
+    for x, y in path:
+        if chart[x][y] != ".":
+            continue
+
+        chart[x][y] = "#"
+
+        exited, _ = map_path(chart)
+        if not exited:
+            obstacles.add((x, y))
+
+        chart[x][y] = "."
+
+    return len(obstacles)
 
 
 with open("input/day06.txt", "r") as f:
     data = [[char for char in line] for line in f.read().splitlines()]
 
-paths = map_path(data, get_coords(data, "^"), (-1, 0))
-print(f"Part 1: {len(set(paths))}")
+_, path = map_path(data)
+print(f"Part 1: {len(path)}")
+
+obstacles = find_new_obstacles(data, path)
+print(f"Part 2: {obstacles}")
